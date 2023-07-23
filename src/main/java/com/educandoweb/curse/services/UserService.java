534eb995139d08557b132c2910f00b065390d4f3
Curso Git GitHub <ruidovalle@gmail.com>
2023-07-23 18:53:42 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/curse/services/UserService.java
@@ -0,0 +1,31 @@
+package com.educandoweb.curse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.educandoweb.curse.entities.User;
+import com.educandoweb.curse.repositories.UserRepository;
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
