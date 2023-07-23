3a1b4402d88c3338ed114c3597c7bb4e247cf72a
Curso Git GitHub <ruidovalle@gmail.com>
2023-07-23 12:13:02 -0300

    Project created

---
+++ src/test/java/com/educandoweb/curse/CurseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.curse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
