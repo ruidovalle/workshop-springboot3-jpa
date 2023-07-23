1c4976f402c06cfe2b43578768833b3fff7606fa
Curso Git GitHub <ruidovalle@gmail.com>
2023-07-23 18:05:39 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/curse/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.curse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.curse.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
